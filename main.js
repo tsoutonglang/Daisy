a8d07c874ad8dbacdc14dd3f839a2fad4e7a9e47
Tania Soutonglang <tsoutonglang12@gmail.com>
2021-06-16 15:44:38 -0500

    added node + base code to get bot running

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('Daisy is online');
+});
+
+client.login('NzM4MTc5MTYxOTYwNjExOTYw.XyII6g.S0CuwsEMZL_W3QjkQQ-wpRSoy9s');

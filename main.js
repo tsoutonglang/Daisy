fa623d1d3f93f59b1b9e7aa7065e28ec6a75d5df
Tania Soutonglang <tsoutonglang12@gmail.com>
2021-06-26 14:09:14 -0500

    added emoji lock to reaction remove

--- main.js
+++ main.js
@@ -85,10 +85,9 @@ client.on('message', message => {
 });
 
 // starboard
-client.on('messageReactionAdd', async (reaction, user) => {
-    const starboard = client.channels.cache.get('854967695455289344'); // channel
-    
+client.on('messageReactionAdd', async (reaction, user) => {  
     if (reaction.emoji.name === '💕'){
+        const starboard = client.channels.cache.get('858422996893302785'); // channel
         const handleStarboard = async ()=> {
             const msgs = await starboard.messages.fetch({ limit: 100 });
             const fetchedMsg = reaction.message;
@@ -147,58 +146,60 @@ client.on('messageReactionAdd', async (reaction, user) => {
 });
 
 client.on('messageReactionRemove', async (reaction, user) => {
-    const starboard = client.channels.cache.get('854967695455289344'); // channel
+    // starboard
+    if (reaction.emoji.name === '💕'){
+        const starboard = client.channels.cache.get('858422996893302785'); // channel
+        const handleStarboard = async ()=> {
+            const msgs = await starboard.messages.fetch({ limit: 100 });
+            const fetchedMsg = reaction.message;
+            const image = fetchedMsg.attachments.size > 0 ? fetchedMsg.attachments.first().url : '';
+            const existingMsg = msgs.find(msg =>
+                msg.embeds.length === 1 ?
+                (msg.embeds[0].footer.text.startsWith(reaction.message.id) ? true : false) : false);
+            
+            if (existingMsg) {
+                const embed = new Discord.MessageEmbed()
+                    .setColor('#f1f48b')
+                    .setAuthor(fetchedMsg.author.tag, fetchedMsg.author.displayAvatarURL())
+                    .setDescription(fetchedMsg.cleanContent)
+                    .addFields(
+                        { name: '**Author**', value : `<@${fetchedMsg.author.id}>`, inline: true},
+                        { name: '**Channel**', value : `${fetchedMsg.channel}`, inline: true},
+                        { name: '**Original**', value : `[Message](${fetchedMsg.url})`},
+                    )
+                    .setFooter(fetchedMsg.id + ' | ' + new Date(fetchedMsg.createdTimestamp).toLocaleString())
+                    .setImage(image);
 
-    const handleStarboard = async ()=> {
-        const msgs = await starboard.messages.fetch({ limit: 100 });
-        const fetchedMsg = reaction.message;
-        const image = fetchedMsg.attachments.size > 0 ? fetchedMsg.attachments.first().url : '';
-        const existingMsg = msgs.find(msg =>
-            msg.embeds.length === 1 ?
-            (msg.embeds[0].footer.text.startsWith(reaction.message.id) ? true : false) : false);
+                existingMsg.edit(`${reaction.count} - 💕`, embed);
+            } else {
+                const embed = new Discord.MessageEmbed()
+                    .setColor('#f1f48b')
+                    .setAuthor(fetchedMsg.author.tag, fetchedMsg.author.displayAvatarURL())
+                    .setDescription(fetchedMsg.cleanContent)
+                    .addFields(
+                        { name: '**Author**', value : `<@${fetchedMsg.author.id}>`, inline: true},
+                        { name: '**Channel**', value : `${fetchedMsg.channel}`, inline: true},
+                        { name: '**Original**', value : `[Message](${fetchedMsg.url})`},
+                    )
+                    .setFooter(fetchedMsg.id + ' | ' + new Date(fetchedMsg.createdTimestamp).toLocaleString())
+                    .setImage(image);
+
+                if (starboard)
+                    starboard.send(`${reaction.count} - 💕`, embed);
+            }
+        }
+
+        if (reaction.message.channel === starboard)
+            return;
         
-        if (existingMsg) {
-            const embed = new Discord.MessageEmbed()
-                .setColor('#f1f48b')
-                .setAuthor(fetchedMsg.author.tag, fetchedMsg.author.displayAvatarURL())
-                .setDescription(fetchedMsg.cleanContent)
-                .addFields(
-                    { name: '**Author**', value : `<@${fetchedMsg.author.id}>`, inline: true},
-                    { name: '**Channel**', value : `${fetchedMsg.channel}`, inline: true},
-                    { name: '**Original**', value : `[Message](${fetchedMsg.url})`},
-                )
-                .setFooter(fetchedMsg.id + ' | ' + new Date(fetchedMsg.createdTimestamp).toLocaleString())
-                .setImage(image);
-
-            existingMsg.edit(`${reaction.count} - 💕`, embed);
-        } else {
-            const embed = new Discord.MessageEmbed()
-                .setColor('#f1f48b')
-                .setAuthor(fetchedMsg.author.tag, fetchedMsg.author.displayAvatarURL())
-                .setDescription(fetchedMsg.cleanContent)
-                .addFields(
-                    { name: '**Author**', value : `<@${fetchedMsg.author.id}>`, inline: true},
-                    { name: '**Channel**', value : `${fetchedMsg.channel}`, inline: true},
-                    { name: '**Original**', value : `[Message](${fetchedMsg.url})`},
-                )
-                .setFooter(fetchedMsg.id + ' | ' + new Date(fetchedMsg.createdTimestamp).toLocaleString())
-                .setImage(image);
-
-            if (starboard)
-                starboard.send(`${reaction.count} - 💕`, embed);
-        }
+            if (reaction.message.partial) {
+                await reaction.fetch();
+                await reaction.message.fetch();
+                handleStarboard();
+            }
+            else
+                handleStarboard();
     }
-
-    if (reaction.message.channel === starboard)
-        return;
-    
-        if (reaction.message.partial) {
-            await reaction.fetch();
-            await reaction.message.fetch();
-            handleStarboard();
-        }
-        else
-            handleStarboard();
 });
 
 client.login('NzM4MTc5MTYxOTYwNjExOTYw.XyII6g.S0CuwsEMZL_W3QjkQQ-wpRSoy9s'); // allows bot to be connected

d5d0ae55b4a5b00135dc3345bd4c15a6def40e98
Tania Soutonglang <tsoutonglang12@gmail.com>
2021-06-27 02:26:23 -0500

    fixed file location

--- commands/kick.js
+++ commands/kick.js
@@ -2,13 +2,15 @@ module.exports = {
     name: 'kick',
     description: "give a kick",
     execute(message, args, Discord){
+        let authorMember = message.member.displayName;
+
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#f1f48b')
         .setImage('https://i.imgur.com/qkrGmV7.gif')
+        .setDescription(`**${authorMember}** kicked someone`);
 
         if (message.mentions.members.first()){
             let targetMember = message.mentions.members.first().displayName;
-            let authorMember = message.member.displayName;
 
             newEmbed.setDescription(`**${authorMember}** kicked **${targetMember}**`)
         }
